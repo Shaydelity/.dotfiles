1b5ce982d2e9bc949fda18f08fe6cc97c0697246
Shaydelity <shaydedg@gmail.com>
2025-06-10 00:31:39 +0200

    Set-up flake

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
- # Zohiu's NixOS flake
+ # Shaydelity's NixOS Flake
 
+Create a folder called ".dotfiles" in your user's home folder and put these files in there to use my NixOS configuration.
 
--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... }:
 let
 
-search = pkgs.writeShellScriptBin "search" ''
-nix search nixpkgs $1
+rebuild = pkgs.writeShellScriptBin "rebuild" ''
+sudo nixos-rebuild $1 --flake ~/.dotfiles#$(hostname)
 '';
+
 
 cava-internal = pkgs.writeShellScriptBin "cava-internal" ''
   cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
@@ -16,7 +17,7 @@ bash -c "swww img $(find ~/.config/home-manager/wallpapers/ -type f | shuf -n 1)
 in
 {
   home.packages = with pkgs; [
-    search
+    rebuild
     cava-internal
     wallpaper_random
   ];
