91bff3237b88ba7afdbf340ec164d353ba8cb334
Shaydelity <shaydedg@gmail.com>
2025-07-15 16:20:06 +0200

    Added Bitwarden

--- home/home.nix
+++ home/home.nix
@@ -35,6 +35,7 @@
   # Also need to rebuild nix to fix dolphin MIME
   home.packages = (with pkgs; [
     # GUI apps
+    bitwarden-desktop
     kitty
     mpv
     blueman
--- hosts/shaydelith/mount.nix
+++ hosts/shaydelith/mount.nix
@@ -4,6 +4,6 @@
   fileSystems."/mnt/T7" = {
     device = "/dev/disk/by-label/T7";
     fsType = "exfat";
-    options = [ "uid=1000" "gid=100" "umask=0002""noatime" "nofail" ];
+    options = [ "uid=1000" "gid=100" "umask=0002" "noatime" "nofail" ];
   };
  }
